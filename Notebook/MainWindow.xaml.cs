3f2dd8efa623233c876596c2781990f44bda6cf3
Voingod-Home <voingod@outlook.com>
2019-07-14 23:57:03 +0300

    Настройка эл-в управления и методов работы с текстом + добавил иконки

--- Notebook/MainWindow.xaml.cs
+++ Notebook/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,11 +24,22 @@ namespace Notebook
         public MainWindow()
         {
             InitializeComponent();
+
+           
+           int countEnum=  Enum.GetNames(typeof(FontSizeSetup)).Length;
+            for (int i=0;i< countEnum;i++)
+            {
+                int numberFontSize = Convert.ToInt32((FontSizeSetup)Enum.GetValues(typeof(FontSizeSetup)).GetValue(i));
+                CmbxSelectionChanged.Items.Add(numberFontSize);
+            }
+
+           // CmbxSelectionChanged.Items.Add("93");
         }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
         }
 
         private void NewFile_Click(object sender, RoutedEventArgs e)
@@ -35,14 +47,47 @@ namespace Notebook
 
         }
 
-        private void CreateFile_Click(object sender, RoutedEventArgs e)
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
         {
-
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.ShowDialog();
         }
 
         private void ExitProgramm_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FontTimesNewRoman_Click(object sender, RoutedEventArgs e)
+        {
+            TxtField.FontFamily = new FontFamily("Times New Roman");
+            FontVerdana.IsChecked = false;
+        }
+
+        private void FontVerdana_Click(object sender, RoutedEventArgs e)
+        {
+            TxtField.FontFamily = new FontFamily("Verdana");
+            FontTimesNewRoman.IsChecked = false;
+        }
+
+        private void SelectionChanged_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
     }
 }
+
+namespace Notebook
+{
+    enum FontSizeSetup
+    {
+        Ten=10,
+        Sixteen=16,
+        Fourteen=14,
+        Twenty=20,
+        TwentyFour=24,
+        ThirtyTwo=32,
+        FourtyEight=48
+    }
+}
+
