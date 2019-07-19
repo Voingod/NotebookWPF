7b6d03e7001557ecdc3ce4cca46d70013620fc1a
Voingod <43098874+Voingod@users.noreply.github.com>
2019-07-19 10:19:37 +0300

    Настроил окно для авторизации

--- Notebook/MainWindow.xaml.cs
+++ Notebook/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,33 +25,43 @@ namespace Notebook
         public MainWindow()
         {
             InitializeComponent();
-
-           
-           int countEnum=  Enum.GetNames(typeof(FontSizeSetup)).Length;
-            for (int i=0;i< countEnum;i++)
+            int countEnum=  Enum.GetNames(typeof(FontSizeSetup)).Length;
+            for (byte i =0;i< countEnum;i++)
             {
-                int numberFontSize = Convert.ToInt32((FontSizeSetup)Enum.GetValues(typeof(FontSizeSetup)).GetValue(i));
+                byte numberFontSize = Convert.ToByte((FontSizeSetup)Enum.GetValues(typeof(FontSizeSetup)).GetValue(i));
                 CmbxSelectionChanged.Items.Add(numberFontSize);
             }
-
-           // CmbxSelectionChanged.Items.Add("93");
+            CmbxSelectionChanged.SelectedItem = Convert.ToByte(FontSizeSetup.Fourteen);
         }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.ShowDialog();
+            bool? res = ofd.ShowDialog();
+            if (res!=false)
+            {
+                Stream stream;
+                if ((stream = ofd.OpenFile()) != null)
+                {
+                    string fileName = ofd.FileName;
+                    TxtField.Text = File.ReadAllText(fileName);
+                }
+            }
         }
 
         private void NewFile_Click(object sender, RoutedEventArgs e)
         {
+            if (TxtField.Text != "")
+            {
+                SaveToFile();
+            }
 
+            TxtField.Text = "";
         }
 
         private void SaveFile_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.ShowDialog();
+            SaveToFile();
         }
 
         private void ExitProgramm_Click(object sender, RoutedEventArgs e)
@@ -72,7 +83,37 @@ namespace Notebook
 
         private void SelectionChanged_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            string cmbxSelectSize = CmbxSelectionChanged.SelectedItem.ToString();
 
+            if (cmbxSelectSize == Convert.ToUInt16(FontSizeSetup.Fourteen).ToString())
+                TxtField.FontSize = Convert.ToUInt16(FontSizeSetup.Fourteen);
+            else if (cmbxSelectSize == Convert.ToUInt16(FontSizeSetup.FourtyEight).ToString())
+                TxtField.FontSize = Convert.ToUInt16(FontSizeSetup.FourtyEight);
+            else if (cmbxSelectSize == Convert.ToUInt16(FontSizeSetup.Sixteen).ToString())
+                TxtField.FontSize = Convert.ToUInt16(FontSizeSetup.Sixteen);
+            else if (cmbxSelectSize == Convert.ToUInt16(FontSizeSetup.Ten).ToString())
+                TxtField.FontSize = Convert.ToUInt16(FontSizeSetup.Ten);
+            else if (cmbxSelectSize == Convert.ToUInt16(FontSizeSetup.ThirtyTwo).ToString())
+                TxtField.FontSize = Convert.ToUInt16(FontSizeSetup.ThirtyTwo);
+            else if (cmbxSelectSize == Convert.ToUInt16(FontSizeSetup.Twenty).ToString())
+                TxtField.FontSize = Convert.ToUInt16(FontSizeSetup.Twenty);
+            else if (cmbxSelectSize == Convert.ToUInt16(FontSizeSetup.TwentyFour).ToString())
+                TxtField.FontSize = Convert.ToUInt16(FontSizeSetup.TwentyFour);
+        }
+        private void SaveToFile()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            bool? res = sfd.ShowDialog();
+            if (res!=false)
+            {
+                using (Stream s = File.Open(sfd.FileName, FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(s))
+                    {
+                        sw.Write(TxtField.Text);
+                    }
+                }
+            }
         }
     }
 }
